2e34589a38c0dbcfdd11b30733cb119276f452d6
benp3837 <petruzziellbj@alumni.vcu.edu>
2025-05-20 10:16:35 -0400

    end of hello angular

--- hello-angular/src/app/components/child-component/child-component.component.ts
+++ hello-angular/src/app/components/child-component/child-component.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-child-component',
@@ -7,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrl: './child-component.component.css'
 })
 export class ChildComponentComponent {
+
+  //We can pass data from a parent to a child with @Input
+  //The value is passed through property binding when the parent RENDERS the child 
+  @Input() index: number = 0;
 
 }
--- hello-angular/src/app/components/parent-component/parent-component.component.ts
+++ hello-angular/src/app/components/parent-component/parent-component.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ChildComponentComponent } from '../child-component/child-component.component';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-parent-component',
-  imports: [CommonModule, ChildComponentComponent], //Parent will render Child, so we gotta import
+  imports: [CommonModule, FormsModule, ChildComponentComponent], //Parent will render Child, so we gotta import
   templateUrl: './parent-component.component.html',
   styleUrl: './parent-component.component.css'
 })
@@ -19,5 +20,15 @@ export class ParentComponentComponent {
   showSurprise():void{
     alert("SURPRISE!!!")
   }
+
+  //defining a variable to toggle the hidden div on and off
+  hideDiv:boolean = true
+
+  toggleHideDiv(){
+    this.hideDiv = !this.hideDiv
+  }
+
+  //defining a variable to be populated by the two way binding
+  name:string = ""
 
 }
